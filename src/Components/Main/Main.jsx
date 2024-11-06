e171f1c9795e06992ea580e066cc7c67a4ea2650
Krzysztof <krisaczi@yahoo.com>
2024-11-06 13:20:14 +0100

    main section layout done

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -19,19 +19,19 @@ const Main = () => {
         ></video>
 
         <div className="className">
-          {" "}
+          <Navbar />
           <div className="bcg"></div>
           <div className="content">
-            <div className="content-text">
-              <h1 className="page-title">Fronted Develoment</h1>
-              <div className="intro">
-                Hello World! 👋 I’m a digital architect with a passion for
-                pixel-perfect designs and coding that brings static pages to
-                life. I craft websites that perform flawlessly and tell
-                compelling stories. Let’s make the web more beautiful and
-                functional, one line of code at a time. Let’s build something
-                amazing together!
-              </div>
+            <div className="content-text"></div>
+          </div>
+          <div className="words">
+            <h1 className="page-title">Fronted Development</h1>
+            <div className="intro">
+              Hello World! 👋 I’m a digital architect with a passion for
+              pixel-perfect designs and coding that brings static pages to life.
+              I craft websites that perform flawlessly and tell compelling
+              stories. Let’s make the web more beautiful and functional, one
+              line of code at a time. Let’s build something amazing together!
             </div>
           </div>
         </div>
