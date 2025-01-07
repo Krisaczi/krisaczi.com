56bdced3c9c0dc0d991db31bf4c0f6a084e0bbb7
Krzysztof <krisaczi@yahoo.com>
2025-01-07 21:58:59 +0100

    size changes

--- src/Components/Stack/Stack.jsx
+++ src/Components/Stack/Stack.jsx
@@ -3,23 +3,26 @@ import "./Stack.css";
 
 const Stack = () => {
   return (
-    <div className="stackWrapper">
-      <div className="contentStack">
-        {stack.map((item) => {
-          return item.display ? (
-            <div className="stackBcg">
-              {" "}
-              <div className="stackBox">
-                <p>{item.technology} </p>
-                <div>
-                  <img className="stackLogo" src={item.logo} alt="" />
+    <div className="stackContainer">
+      <div className="stackEffect"></div>
+      <div className="stackWrapper">
+        <div className="contentStack">
+          {stack.map((item) => {
+            return item.display ? (
+              <div className="stackBcg">
+                {" "}
+                <div className="stackBox">
+                  <p>{item.technology} </p>
+                  <div>
+                    <img className="stackLogo" src={item.logo} alt="" />
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            ""
-          );
-        })}
+            ) : (
+              ""
+            );
+          })}
+        </div>
       </div>
     </div>
   );
